f1d5f7b901ed5fef544a5c0718994a5fbd45197c
akashr206 <akashr6514@gmail.com>
2024-09-06 23:19:26 +0530

    Update home-display.js

--- static/scripts/home-display.js
+++ static/scripts/home-display.js
@@ -2,7 +2,7 @@
 
 let categoryItems = [
     {cName:"Nandini",page:"nandini.html",img:"../logo/nandini.jpg"},
-    {cName:"Dodla",page:"Dodla.html",img:"../logo/dodla.jpg"},
+    {cName:"Dodla",page:"dodla.html",img:"../logo/dodla.jpg"},
     {cName:"Milk",page:"milk.html",img:"../logo/milk.jpg"},
     {cName:"Curd",page:"nandini.html",img:"../logo/curd.jpg"},
     {cName:"Others",page:"nandini.html",img:"../logo/other.jpg"},
